9a0f55bfc98e410431c25d4ab6ae56673445843d
Treee <jonathon.lindemann@gmail.com>
2019-08-23 11:15:11 -0600

    adding agent, and package lock

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 
-// export { Agent } from './agent/agent';
-
-// export { Breeder } from './breeder/breeder';
-
-// export { Simulator } from './simulator/simulator';
+export { Agent } from './agent/agent';
